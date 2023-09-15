92b4525c135a7653499a3ca0bad286b066f400ed
dmropp <134107540+dmropp@users.noreply.github.com>
2023-09-15 13:37:54 -0700

    initialize bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -4,5 +4,53 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 // console.log("Data Promise", dataPromise);
 
 d3.json(url).then(function(data) {
+    
     console.log(data);
+
+    let patients = Object.values(data.samples);
+    console.log(patients);
+
+    let labels = Object.keys(data.samples[0]);
+    console.log(labels);
+
+    let patient = Object.values(data.samples[0]);
+    console.log(patient);
+
+    let sampleValues = Object.values(data.samples[0].sample_values);
+    console.log(sampleValues);
+
+
+
+    function init() {
+        let plotData = [{
+            x: Object.values(data.samples[0].sample_values.slice(0, 10)),
+            y: Object.values(data.samples[0].otu_ids).slice(0, 10),
+            type: "bar",
+            orientation: "h"
+        }];
+
+        let layout = {
+            height: 600,
+            width: 400
+        }
+
+        Plotly.newPlot("plot", plotData, layout);
+    }
+
+    init();
+
 });
+
+// console.log("Hello");
+
+// let samples = Object.values(bellyButtonData);
+// console.log(samples);
+
+// let entries = Object.keys(bellyButtonData);
+// console.log(entries);
+
+// function.init() {
+//     let data = [{
+//         x: 
+//     }]
+// }
