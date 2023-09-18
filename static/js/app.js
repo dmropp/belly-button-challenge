a2b0be7eaa5ec733928d8fe646bd3cb3c820cca6
dmropp <134107540+dmropp@users.noreply.github.com>
2023-09-18 11:17:51 -0700

    Sample y axis label for loop created

--- static/js/app.js
+++ static/js/app.js
@@ -19,36 +19,25 @@ d3.json(url).then(function(data) {
     let sampleValues = Object.values(data.samples[0].sample_values);
     console.log(sampleValues);
 
-    // Function to create array of otu labels for the chart as the values are stored as INTs and not strings in the array
-    
-    // function yAxisLabels(yLabels) {
-    //     for (let i = 0, i < yLabels.length, i++) {
-    //         let otu_labels = yLabels[i];
-
-    //         return otu_labels;
-    //     } 
-    // } 
-
-
     function init() {
         
-        // Function to create array of otu labels for the chart as the values are stored as INTs and not strings in the original array
+        // For loop to create array of otu labels for the chart as the values are stored as INTs and not strings in the original array
 
-        let sampleOTU = Object.values(data.samples[0].otu_ids).slice(0, 10);
+        let sampleOTU = Object.values(data.samples[0].otu_ids).slice(0, 10).reverse();
 
         let sampleOTUArray = [];
         
-        for (let i = 0, i < sampleOTU.length, i++) {
+        for (let i = 0; i < sampleOTU.length; i++) {
 
-            otu = sampleOTU[i];
+            otu = `OSU ${sampleOTU[i]}`;
             sampleOTUArray.push(otu);
 
         }
 
         let plotData = [{
-            x: Object.values(data.samples[0].sample_values.slice(0, 10)),
+            x: Object.values(data.samples[0].sample_values.slice(0, 10)).reverse(),
             // y: Object.values(data.samples[0].otu_ids).slice(0, 10),
-            y: sampleOTUArray;
+            y: sampleOTUArray,
             type: "bar",
             orientation: "h"
         }];
@@ -66,17 +55,3 @@ d3.json(url).then(function(data) {
     init();
 
 });
-
-// console.log("Hello");
-
-// let samples = Object.values(bellyButtonData);
-// console.log(samples);
-
-// let entries = Object.keys(bellyButtonData);
-// console.log(entries);
-
-// function.init() {
-//     let data = [{
-//         x: 
-//     }]
-// }
